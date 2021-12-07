37c49bf5a0477c4148f4476e9ed18d35a9509312
daniel.tabach@baruchmail.cuny.edu <daniel.tabach@baruchmail.cuny.edu>
2021-12-07 00:40:46 +0000

    Sorry - Finished ETL Now

---
+++ models/collision_fact.sql
@@ -0,0 +1,8 @@
+select date_dim_id, time_dim_id, location_dim_id, unique_key, number_of_cyclist_injured, 
+number_of_cyclist_killed, number_of_motorist_injured, number_of_motorist_killed,
+number_of_pedestrians_injured, number_of_pedestrians_killed,number_of_persons_injured,number_of_persons_killed
+from `data-project-329222.newyork_311.collisions` as col
+    inner join {{ref('date_dimension')}}as date_dimension on date_dimension.DATE_DAY = date(col.timestamp)
+    inner join {{ref('location_dimension')}} as location_dimension on location_dimension.Borough = col.borough 
+    and cast(location_dimension.zipcode as int) =col.zip_code
+    inner join `data-project-329222.newyork_311.time` as time_dimension on time_dimension.fulltime = time(col.timestamp)
---
+++ models/complaint_fact.sql
@@ -0,0 +1,24 @@
+SELECT unique_key,
+created_date_dim.DATE_DIM_ID as Created_Date_Dim_ID,
+closed_date_dim.DATE_DIM_ID as Closed_Date_Dim_ID, 
+agency_dim_id, 
+complaint_dim_id, 
+time_dimension_closed.time_dim_id as closed_time, 
+time_dimension_open.time_dim_id as open_time,
+location_dimension.location_dim_id,
+FROM `data-project-329222.newyork_311.311-service-requests` AS sr
+INNER JOIN {{ref('date_dimension')}} AS created_date_dim 
+On created_date_dim.DATE_DAY = date(sr.created_date)
+INNER JOIN {{ref('date_dimension')}} AS closed_date_dim 
+On closed_date_dim.DATE_DAY = date(sr.closed_date)
+INNER JOIN {{ref('agency_dimension')}} AS agency_dim
+On agency_dim.agency = sr.agency AND agency_dim.agency_name = sr.agency_name
+INNER JOIN {{ref('complaint_dimension')}} AS complaint_dim ON
+complaint_dim.complaint_type = sr.complaint_type AND complaint_dim.descriptor =
+sr.descriptor
+inner join {{ref('location_dimension')}} as location_dimension on location_dimension.Borough = sr.borough
+and cast(location_dimension.zipcode as string) = cast(sr.incident_zip as string)
+inner join `data-project-329222.newyork_311.time` as time_dimension_open on time_dimension_open.fulltime = time(sr.created_date)
+inner join `data-project-329222.newyork_311.time` as time_dimension_closed on time_dimension_closed.fulltime = time(sr.closed_date)
+
+
